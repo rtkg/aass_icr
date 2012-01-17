ad40d2b9b53566df3232dc7ce167958d74e2ec7c
Robert Krug <robert.krug@oru.se>
2012-01-17 13:18:42 +0100

    put the icr computation in a callback function compute

--- icr/src/icr_server.cpp
+++ icr/src/icr_server.cpp
@@ -5,27 +5,15 @@
 #include <sys/time.h>
 #include <time.h>
 #include "icr/compute_icr.h"
+#include <algorithm>
+#include <iterator>
 
 bool compute(icr::compute_icr::Request  &req,
 	     icr::compute_icr::Response &res )
 {
-  std::cout<<"In compute function"<<std::endl;
-
-
-  ROS_INFO("sending back response: [%u]", (bool)res.success);
-  return true;
-}
-
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "icr_srv");
-  ros::NodeHandle n;
-
- 
-  ros::ServiceServer service = n.advertiseService("compute_icr", compute);
-  ROS_INFO("Ready to compute icr.");
-
+  std::stringstream input;
+  std::copy(req.centerpoint_ids.begin(),req.centerpoint_ids.end(), std::ostream_iterator<unsigned int>(input," ")); 
+  ROS_INFO("Given centerpoint id's: %s", input.str().c_str());
 
   //Load a new target object 
   ICR::ObjectLoader obj_loader;
@@ -38,8 +26,10 @@ int main(int argc, char **argv)
    for (int i=0;i<5;i++)
      f_parameters.push_back(parameters);
 
-   ICR::VectorXui centerpoint_ids(5);
-   centerpoint_ids << 1838, 4526, 4362, 1083, 793;
+   ICR::VectorXui centerpoint_ids(req.centerpoint_ids.size());
+   
+   for(unsigned int i=0; i<req.centerpoint_ids.size();i++)
+       centerpoint_ids(i)=req.centerpoint_ids[i];  //centerpoint_ids e.g. [1838, 4526, 4362, 1083, 793] for the cup
 
    //Modify some of the finger parameters of the prototype-grasp.
    //The third finger utilizies the Multi-Point contact model, patches only contain the center-point
@@ -52,6 +42,14 @@ int main(int argc, char **argv)
    //origin-centered ball contained by the Grasp wrench space of the prototype grasp
    ICR::GraspPtr prototype_grasp(new ICR::Grasp());
    prototype_grasp->init(f_parameters,obj_loader.getObject(),centerpoint_ids);
+
+   if(!prototype_grasp->getGWS()->containsOrigin())
+     {
+       ROS_INFO("Given prototype grasp is not force closure - Not possible to compute ICR");
+       res.success=false;
+       return res.success;
+     }
+
    ICR::SearchZonesPtr search_zones(new ICR::SearchZones(prototype_grasp));
    double alpha=0.5;
    search_zones->computeShiftedSearchZones(alpha);
@@ -60,6 +58,9 @@ int main(int argc, char **argv)
    ICR::IndependentContactRegions icr(search_zones,prototype_grasp);
    icr.computeICR();
    std::cout<<icr;
+
+   res.success=true;
+  return res.success;
 
   // Utility for timing selected parts of the code - uncomment below and put the code to be timed at the marked location
   // struct timeval start, end;
@@ -72,6 +73,15 @@ int main(int argc, char **argv)
   // c_time = end.tv_sec - start.tv_sec + 0.000001 * (end.tv_usec - start.tv_usec);
   // std::cout<<"Computation time: "<<c_time<<" s"<<std::endl;
 
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "icr_srv");
+  ros::NodeHandle n;
+  ros::ServiceServer service = n.advertiseService("compute_icr", compute);
+  ROS_INFO("Ready to compute ICR");
   ros::spin();
 
   return 0;
