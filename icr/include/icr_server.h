99c510c8823775d5fff1f4a3d6c3c4fbf44a054c
Robert Krug <robert.krug@oru.se>
2012-01-18 10:56:43 +0100

    modified the icr server

---
+++ icr/include/icr_server.h
@@ -0,0 +1,33 @@
+#ifndef icr_server_h___
+#define icr_server_h___
+
+#include "ros/ros.h"
+#include "icr.h"
+#include "../srv_gen/cpp/include/icr/compute_icr.h"
+#include "../srv_gen/cpp/include/icr/load_object.h"
+#include <boost/thread/mutex.hpp>
+
+
+class IcrServer
+{
+private:
+
+  ros::NodeHandle nh_;
+  ros::ServiceServer compute_icr_service_;
+  ros::ServiceServer load_wfront_obj_service_;
+  ICR::ObjectLoader* obj_loader_;
+  boost::mutex data_mutex_;
+
+public:
+ 
+  IcrServer();
+  ~IcrServer();
+
+  bool computeIcr(icr::compute_icr::Request  &req, icr::compute_icr::Response &res);
+  bool loadWfrontObj(icr::load_object::Request  &req, icr::load_object::Response &res);
+ 
+};
+
+
+
+#endif
--- icr/src/icr_server.cpp
+++ icr/src/icr_server.cpp
@@ -1,22 +1,35 @@
-#include "ros/ros.h"
-#include "icr.h"
-#include "icr/compute_icr.h"
-#include "icr/load_object.h"
+#include "../include/icr_server.h"
 #include <sys/time.h>
 #include <time.h>
-#include <boost/thread/mutex.hpp>
 
-bool compute(icr::compute_icr::Request  &req,
-	     icr::compute_icr::Response &res)
+//----------------------------------------------------------------------------------------
+IcrServer::IcrServer() : obj_loader_(new ICR::ObjectLoader()) 
+  {
+    compute_icr_service_=nh_.advertiseService("/icr_server/compute_icr",&IcrServer::computeIcr,this);
+    load_wfront_obj_service_=nh_.advertiseService("/icr_server/load_wfront_obj",&IcrServer::loadWfrontObj,this);
+
+    std::cout<<"Constructor of IcrServer"<<std::endl;
+  }
+//----------------------------------------------------------------------------------------
+IcrServer::~IcrServer()
 {
+  delete obj_loader_;
+}
+//----------------------------------------------------------------------------------------
+bool IcrServer::computeIcr(icr::compute_icr::Request  &req, icr::compute_icr::Response &res)
+{
+
+  if(!obj_loader_->objectLoaded())
+    {
+    ROS_INFO("A valid target object needs to be loaded prior to the ICR computation.");
+    res.success=false;
+    return res.success;
+    }
+
   std::stringstream input;
   std::copy(req.centerpoint_ids.begin(),req.centerpoint_ids.end(), std::ostream_iterator<unsigned int>(input," ")); 
-  ROS_INFO("Given centerpoint id's: %s", input.str().c_str());
+  ROS_INFO("Computing ICR for object: %s with given centerpoint id's: %s",(obj_loader_->getObject()->getName()).c_str(),input.str().c_str());
 
-  //Load a new target object 
-  ICR::ObjectLoader obj_loader;
-  obj_loader.loadObject("/home/rkg/ros/aass_icr/libicr/icrcpp/models/beer_can.obj","beer_can");
-   
   //Create a list of 5 default finger parameters (default parameters defined in config.h) and a 
   //vector of centerpoint contact id's for the 5-fingered prototype grasp
   ICR::FParamList f_parameters;
@@ -39,7 +52,7 @@ bool compute(icr::compute_icr::Request  &req,
    //Create a prototype grasp and search zones, the parameter alpha is the scale of the largest
    //origin-centered ball contained by the Grasp wrench space of the prototype grasp
    ICR::GraspPtr prototype_grasp(new ICR::Grasp());
-   prototype_grasp->init(f_parameters,obj_loader.getObject(),centerpoint_ids);
+   prototype_grasp->init(f_parameters,obj_loader_->getObject(),centerpoint_ids);
 
    if(!prototype_grasp->getGWS()->containsOrigin())
      {
@@ -70,47 +83,8 @@ bool compute(icr::compute_icr::Request  &req,
   // gettimeofday(&end,0);
   // c_time = end.tv_sec - start.tv_sec + 0.000001 * (end.tv_usec - start.tv_usec);
   // std::cout<<"Computation time: "<<c_time<<" s"<<std::endl;
-
 }
-
-
-class IcrServer
-{
-private:
-
-  ros::NodeHandle nh_;
-  ros::ServiceServer compute_icr_service_;
-  ros::ServiceServer load_wfront_obj_service_;
-  ICR::ObjectLoader* obj_loader_;
-  boost::mutex data_mutex_;
-public:
- 
-  IcrServer();
-  ~IcrServer();
-  bool computeIcr(icr::compute_icr::Request  &req, icr::compute_icr::Response &res);
-  bool loadWfrontObj(icr::load_object::Request  &req, icr::load_object::Response &res);
- 
-};
-
-IcrServer::IcrServer() : obj_loader_(new ICR::ObjectLoader()) 
-  {
-    //    compute_icr_service_=nh_.advertiseService("compute_icr",&IcrServer::computeIcr,this);
-    load_wfront_obj_service_=nh_.advertiseService("load_wfront_obj",&IcrServer::loadWfrontObj,this);
-
-    std::cout<<"Constructor of IcrServer"<<std::endl;
-  }
-
-IcrServer::~IcrServer()
-{
-  delete obj_loader_;
-}
-bool IcrServer::computeIcr(icr::compute_icr::Request  &req, icr::compute_icr::Response &res)
-{
-
-
-  std::cout<<"computing"<<std::endl;
-  return res.success;
-}
+//----------------------------------------------------------------------------------------
 bool IcrServer::loadWfrontObj(icr::load_object::Request  &req, icr::load_object::Response &res)
 {
   data_mutex_.lock();
@@ -124,204 +98,18 @@ bool IcrServer::loadWfrontObj(icr::load_object::Request  &req, icr::load_object:
   return res.success;
   data_mutex_.unlock();
 }
-
-
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "icr_srv");
   ros::NodeHandle n;
 
-  ros::ServiceServer compute_icr_service = n.advertiseService("compute_icr", compute);
+  //  ros::ServiceServer compute_icr_service = n.advertiseService("compute_icr", compute);
   IcrServer server;
-  ROS_INFO("Ready to compute ICR");
+  ROS_INFO("ICR server ready");
   ros::spin();
 
   return 0;
 }
-
-
-//some dummy changes
-// include <PointCloudUtils.hh>
-// #include <cstdio>
-// #include <fstream>
-// #include <Eigen/Eigen>
-
-// #include "ros/ros.h"
-// #include "pcl/point_cloud.h"
-// #include "sensor_msgs/PointCloud2.h"
-// #include "pcl/io/pcd_io.h"
-// #include "message_filters/subscriber.h"
-// #include "tf/message_filter.h"
-// #include <tf/transform_broadcaster.h>
-// #include <tf/transform_listener.h>
-
-/// A shamelessly hardcoded logging node....
-
-// class LoggerNode
-// {
-// protected:
-//   // Our NodeHandle
-//   ros::NodeHandle nh_;
-
-//   ros::Subscriber kinect_;
-//   ros::Subscriber sr_;
-//   ros::Subscriber fotonic_;
-//   ros::Subscriber laser_;
-//   ros::Time lastLaser;
-
-//   tf::TransformListener tfListener;
- 
-//   // Use the vector as a cyclic buffer (increment with std::rotate).
-//   pcl::PointCloud<pcl::PointXYZ> sensor1_pc,sensor2_pc,sensor3_pc,sensor4_pc;
-
-//   boost::mutex data_mutex;
-//   bool s1_fresh, s2_fresh, s3_fresh, s4_fresh;
-//   int dump_number;
-//   std::string laserFrame, fotonicFrame, srFrame, kinectFrame;
-
-// public:
-//      // Constructor
-//      LoggerNode() : s1_fresh(0), s2_fresh(0), s3_fresh(0), s4_fresh(0), dump_number(0)
-//     {
-
-// 	kinect_ = nh_.subscribe("/logger/kinect/points", 10, &LoggerNode::s1pts, this);
-// 	sr_ = nh_.subscribe("/logger/sr/points", 10, &LoggerNode::s2pts, this);
-// 	fotonic_ = nh_.subscribe("/logger/fotonic/points", 10, &LoggerNode::s3pts, this);
-// 	laser_ = nh_.subscribe("/logger/laser/points", 10, &LoggerNode::s4pts, this);
-	
-// 	laserFrame = "/laser_frame";
-// 	kinectFrame = "/openni_rgb_optical_frame";
-// 	srFrame = "/camera";
-// 	fotonicFrame = "/fotonic_frame";
-// 	tfListener.setExtrapolationLimit(ros::Duration(1));
-//     }
- 
-//   //dumps the fresh point clouds
-//   void log();
-
-//   // Callback
-//   void s1pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in);
-//   void s2pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in);
-//   void s3pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in);
-//   void s4pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in);
-
-// public:
-//   EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-// };
-//======================================================================
-//======================================================================
-// #include <LoggerNode.hh>
-// #include <pcl/registration/transformation_estimation_svd.h>
-// #include <tf_conversions/tf_eigen.h>
-
-// #define DO_DEBUG_PROC
-
-// void LoggerNode::log(){
-    
-//     //dump the point clouds with corresponding points (debug)
-//     char fname[100];
-//     Eigen::Transform<double,3,Eigen::Affine, Eigen::ColMajor> TR;
-//     TR.setIdentity();
-//     double xoffset, yoffset, zoffset, roll, pitch, yaw;
-
-//     tf::StampedTransform transformLaser, transformKinect, transformSR, transformFotonic;
-    
-//     tfListener.lookupTransform("/robot_frame", laserFrame, lastLaser, transformLaser);
-//     tfListener.lookupTransform("/robot_frame", kinectFrame, lastLaser, transformKinect);
-//     tfListener.lookupTransform("/robot_frame", srFrame, lastLaser, transformSR);
-//     tfListener.lookupTransform("/robot_frame", fotonicFrame, lastLaser, transformFotonic);
-
-
-//     //sensor1 -- kinect
-//     //kinect points go in without tranformations
-//     tf::TransformTFToEigen(transformKinect,TR);
-//     lslgeneric::transformPointCloudInPlace(TR, sensor1_pc);
-//     snprintf(fname,99,"sensorlog/kinect/pc%04d.wrl",dump_number);
-//     FILE *fout = fopen(fname,"w");
-//     if(fout != NULL) {
-// 	fprintf(fout,"#VRML V2.0 utf8\n");
-// 	lslgeneric::writeToVRML(fout,sensor1_pc);
-//     }
-//     fclose(fout);
- 
-//     //sensor 2 -- swiss ranger
-//     tf::TransformTFToEigen(transformSR,TR);
-//     lslgeneric::transformPointCloudInPlace(TR, sensor2_pc);
-//     snprintf(fname,99,"sensorlog/sr/pc%04d.wrl",dump_number);
-//     fout = fopen(fname,"w");
-//     if(fout != NULL) {
-// 	fprintf(fout,"#VRML V2.0 utf8\n");
-// 	lslgeneric::writeToVRML(fout,sensor2_pc);
-//     }
-//     fclose(fout);
-    
-//     //sensor 3 -- fotonic camera
-//     tf::TransformTFToEigen(transformFotonic,TR);
-//     lslgeneric::transformPointCloudInPlace(TR, sensor3_pc);
-//     snprintf(fname,99,"sensorlog/fotonic/pc%04d.wrl",dump_number);
-//     fout = fopen(fname,"w");
-//     if(fout != NULL) {
-// 	fprintf(fout,"#VRML V2.0 utf8\n");
-// 	lslgeneric::writeToVRML(fout,sensor3_pc);
-//     }
-//     fclose(fout);
-    
-//     //sensor 4 -- actuated laser
-//     tf::TransformTFToEigen(transformLaser,TR);
-//     lslgeneric::transformPointCloudInPlace(TR, sensor4_pc);
-//     snprintf(fname,99,"sensorlog/laser/pc%04d.wrl",dump_number);
-//     fout = fopen(fname,"w");
-//     if(fout != NULL) {
-// 	fprintf(fout,"#VRML V2.0 utf8\n");
-// 	lslgeneric::writeToVRML(fout,sensor4_pc);
-//     }
-//     fclose(fout);
-    
-//     //combined
-//     snprintf(fname,99,"sensorlog/all/pc%04d.wrl",dump_number);
-//     fout = fopen(fname,"w");
-//     if(fout != NULL) {
-// 	fprintf(fout,"#VRML V2.0 utf8\n");
-// 	lslgeneric::writeToVRML(fout,sensor1_pc,Eigen::Vector3d(1,1,1));
-// 	lslgeneric::writeToVRML(fout,sensor2_pc,Eigen::Vector3d(1,0,0));
-// 	lslgeneric::writeToVRML(fout,sensor3_pc,Eigen::Vector3d(0,1,0));
-// 	lslgeneric::writeToVRML(fout,sensor4_pc,Eigen::Vector3d(0,0,1));
-//     }
-//     fclose(fout);
-
-//     dump_number++;
-//     s1_fresh = false; s2_fresh = false;
-//     s3_fresh = false; s4_fresh = false;
-// }
-
-
-// // Callback
-// void LoggerNode::s1pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in) {
-//     data_mutex.lock();
-//     pcl::fromROSMsg (*msg_in, sensor1_pc);
-//     s1_fresh = true;
-//     if(s1_fresh && s2_fresh && s3_fresh && s4_fresh) log(); 
-//     data_mutex.unlock();
-// }
-// void LoggerNode::s2pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in) {
-//     data_mutex.lock();
-//     pcl::fromROSMsg (*msg_in, sensor2_pc);
-//     s2_fresh = true;
-//     if(s1_fresh && s2_fresh && s3_fresh && s4_fresh) log(); 
-//     data_mutex.unlock();
-// }
-// void LoggerNode::s3pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in) {
-//     data_mutex.lock();
-//     pcl::fromROSMsg (*msg_in, sensor3_pc);
-//     s3_fresh = true;
-//     if(s1_fresh && s2_fresh && s3_fresh && s4_fresh) log(); 
-//     data_mutex.unlock();
-// }
-// void LoggerNode::s4pts(const sensor_msgs::PointCloud2::ConstPtr& msg_in) {
-//     data_mutex.lock();
-//     pcl::fromROSMsg (*msg_in, sensor4_pc);
-//     s4_fresh = true;
-//     lastLaser = msg_in->header.stamp;
-//     if(s1_fresh && s2_fresh && s3_fresh && s4_fresh) log(); 
-//     data_mutex.unlock();
-// }
+//----------------------------------------------------------------------------------------
